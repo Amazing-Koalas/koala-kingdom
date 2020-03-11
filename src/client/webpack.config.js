71f5d6c99eff85303540a36b112a9320bcd7b161
Jemal <53106252+Jebdi@users.noreply.github.com>
2020-03-10 19:29:34 -0500

    hi

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -1,66 +1,34 @@
 const path = require("path");
-const ManifestPlugin = require("webpack-manifest-plugin");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
-const { SERVER_PORT, WEBPACK_PORT } = require("../../config/client");
+const isProd = process.env.NODE_ENV === "production";
 
-const nodeModulesPath = path.resolve(__dirname, "node_modules");
-const outputPath = path.resolve(__dirname, "dist");
-const isProd = process.env.NODE_ENV || "production";
-
-// TODO: This might be a good idea; Part of babel; Do we want to use babel?
-// const targets = IS_DEV ? { chrome: "79", firefox: "72" } : "> 0.25%, not dead";
-
-const plugins = [
-  new CleanWebpackPlugin({
-    cleanOnceBeforeBuildPatterns: [outputPath]
-  }),
-  new HTMLWebpackPlugin({
-    template: path.resolve(__dirname, "src/index.html")
-  }),
-  // new CopyWebpackPlugin([]), // TODOO: copy over game assets, images, css, etc
-  new ManifestPlugin(),
-  new CopyWebpackPlugin([{ from: "src/assets", to: "assets" }])
-];
-
-const config = {
+module.exports = {
   entry: "./src/index.ts",
   mode: isProd ? "production" : "development",
-  devtool: "IS_DEV" ? "inline-source-map" : false,
   resolve: {
-    extensions: ["tsx", ".ts", ".js", ".json"]
+    extensions: [".ts", ".js", ".json"]
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: "ts-loader",
+        test: /\.ts$/,
+        use: [{ loader: "awesome-typescript-loader" }],
         exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
       }
     ]
   },
   output: {
-    filename: "[name]-[hash:8]-bundle.js",
-    sourceMapFilename: "[file].map",
-    path: outputPath
+    filename: "app.[contenthash].js",
+    path: path.resolve(__dirname, "dist")
   },
-  devServer: {
-    contentBase: outputPath,
-    // hot: true,
-    allowedHosts: ["."]
-  },
-  plugins,
-  optimization: {
-    splitChunks: {
-      chunks: "all"
-    }
-  }
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html")
+    }),
+    // Pixi will manage game asset loading, so we just need to copy them.
+    // Webpack will not be involved in bundling.
+    new CopyPlugin([{ from: "src/assets", to: "assets" }])
+  ]
 };
-
-module.exports = config;
