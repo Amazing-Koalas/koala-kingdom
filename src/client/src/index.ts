f5cf9db03be4cdd03051c6e08f79495e5b042149
Jemal <53106252+Jebdi@users.noreply.github.com>
2020-04-26 10:30:14 -0500

    Accessibility Functionality Added, Text Style Altered

--- src/client/src/index.ts
+++ src/client/src/index.ts
@@ -1,9 +1,9 @@
 import {
-    getCanvasEl,
-    createPixiApp,
-    loadPixiAssets,
-    initializeComponents,
-    GameContainer,
+  getCanvasEl,
+  createPixiApp,
+  loadPixiAssets,
+  initializeComponents,
+  GameContainer,
 } from "./framework";
 import { Scene, Textures, World } from "./constants";
 import { Character } from "./components/Character";
@@ -12,40 +12,29 @@ import { State } from "./components/State";
 import { Menu } from "./components/Menu";
 import { initState } from "./state";
 
-
 const initGame = async () => {
+  const [_, level] = await Promise.all([loadPixiAssets(Textures)]);
 
-    const [_, level] = await Promise.all([loadPixiAssets(Textures)]);
-    //document.getElementById('startScreen').style.display = 'none';
+  const canvasEl = getCanvasEl("game");
+  canvasEl.height = Scene.Height;
+  canvasEl.width = Scene.Width;
 
-    const canvasEl = getCanvasEl("game");
-    canvasEl.height = Scene.Height;
-    canvasEl.width = Scene.Width;
+  const pixiApp = createPixiApp({
+    view: canvasEl,
+    width: Scene.Width,
+    height: Scene.Height,
+  });
 
-    const pixiApp = createPixiApp({
-        view: canvasEl,
-        width: Scene.Width,
-        height: Scene.Height,
-    });
+  const initializer = initializeComponents(
+    pixiApp,
+    [Background, Menu, State, Character],
+    initState({})
+  );
 
+  initializer();
 
-    /*const initializer = initializeComponents(
-        pixiApp,
-        [State, Background, Character],
-        initState({})
-    );*/
-
-    const startGame  = initializeComponents(pixiApp,[Background, Menu, State, Character],initState({}));
-    
-    //initializer();
-    startGame();
-    
-    GameContainer.children[3].visible = false;
-    console.log(GameContainer.children);
-    //pixiApp.stage.children[0].visible = false;
-
+  GameContainer.getChildByName("Character").visible = false;
+  //console.log(GameContainer.children);
 };
 
-//let btn = document.getElementById("startBtn");
-//btn.onclick = function(){initGame()};
 initGame();
